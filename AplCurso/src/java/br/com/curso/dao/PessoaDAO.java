369217c8359ab3c3c31cec63acb426311c593379
Mateus Ferreira <matefe1200@gmail.com>
2023-11-06 10:49:19 -0300

    Alteração na camada PessoaDao

--- AplCurso/src/java/br/com/curso/dao/PessoaDAO.java
+++ AplCurso/src/java/br/com/curso/dao/PessoaDAO.java
@@ -7,6 +7,9 @@ package br.com.curso.dao;
 import br.com.curso.model.Pessoa;
 import br.com.curso.utils.SingleConnection;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.ParseException;
 
 /**
@@ -35,5 +38,58 @@ public class PessoaDAO {
             retorno = this.alterar(oPessoa);
         }
         return retorno;
+    }
+    
+    public int inserir (Object objeto) {
+      Pessoa oPessoa = (Pessoa) objeto;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Integer IdPessoa = null;
+        String sql = "insert into pessoa (cpfcnpj,nome,datanascimento,idcidade,login,senha,foto) values (?,?,?,?,?,?,?) returning IdPessoa;" ;
+        try{
+            stmt = conexao.prepareStatement(sql);
+            stmt.setString(1, oPessoa.getCpfCnpj());
+            stmt.setString(2, oPessoa.getNome());
+            stmt.setDate(3, new java.sql.Date(oPessoa.getDataNascimento().getTime()));
+            stmt.setInt(4, oPessoa.getCidade().getIdCidade());
+            stmt.setString(5, oPessoa.getLogin());
+            stmt.setString(6, oPessoa.getSenha());
+            stmt.setString(7, oPessoa.getFoto());
+            rs = stmt.executeQuery();
+            
+            while (rs.next()) {
+                IdPessoa = rs.getInt(IdPessoa);
+            }
+        } catch(SQLException e){
+            try{
+                System.out.println("Problemas ao cadastrar pessoas. Erro: "+e.getMessage());
+                e.printStackTrace();
+                conexao.rollback();
+            } catch(SQLException ex) {
+                System.out.println("Problemas ao executar o hollback"+ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+        return IdPessoa;
+    }
+    
+    public int alterar(Object objeto){
+        Pessoa oPessoa = (Pessoa) objeto;
+        PreparedStatement stmt = null;
+        Integer idPessoa = oPessoa.getIdPessoa();
+        String sql = "";
+        try{
+            stmt = conexao.prepareStatement(sql);
+        } catch (SQLException e){
+            try{
+                System.out.println("Problemas ao alterar Pessoa. Erro"+ e.getLocalizedMessage());
+                e.printStackTrace();
+                conexao.rollback();
+            } catch(SQLException ex){
+                System.out.println("Problemas ao realizar o rollback. erro:"+ ex.getMessage());
+                ex.printStackTrace();
+            }
+        }
+        return idPessoa;
     }
 }
